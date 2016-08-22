f6b7b36ce40aff090bfcdced384cc052663a96ca
c1505 <coreym247@gmail.com>
2016-08-22 21:33:33 +0000

    all tests passing

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -1,55 +1,37 @@
-require 'pry'
-class Bst
-    attr_reader :left, :right, :data
-    def initialize(data)
-      @data = data
-      @left
-      @right
-    end
-    
-    def insert(num)
-      if @data >= num
-        if @left.nil?
-          @left = Bst.new(num)
-        else
-          @left.insert(num)
-        end
-      elsif @data < num
-        if @right.nil?
-          @right = Bst.new(num)
-        else
-          @right.insert(num)
-        end
-      end
-    end
-    #proceed down until target is nil
-    
-end
 
-class Node
-    attr_reader :left, :right, :data
-    def initialize(data)
-      @data = data
-      @left
-      @right
-    end
-    
-    def insert(num)
-      if @data >= num
+class Bst
+  VERSION = 1
+  attr_reader :left, :right, :data
+  def initialize(data)
+    @data = data
+    @left
+    @right
+  end
+  
+  def insert(num)
+    if @data >= num
+      if @left.nil?
         @left = Bst.new(num)
-      elsif @data < num
+      else
+        @left.insert(num)
+      end
+    elsif @data < num
+      if @right.nil?
         @right = Bst.new(num)
+      else
+        @right.insert(num)
       end
     end
   end
+  
+  def each(&block)
+    return to_enum(__method__) unless block_given?
+    @left.each(&block) if @left
+    block.call(@data)
+    @right.each(&block) if @right
+  end
+  
 
-# should i have a node class?
 
-# A binary search tree consists of a series of connected nodes. Each node
-# contains a piece of data (e.g. the number 3), a variable named `left`,
-# and a variable named `right`. The `left` and `right` variables point at
-# `nil`, or other nodes. Since these other nodes in turn have other nodes
-# beneath them, we say that the left and right variables are pointing at
-# subtrees. All data in the left subtree is less than or equal to the
-# current node's data, and all data in the right subtree is greater than
-# the current node's data.
+    
+end
--- binary_search_tree_test.rb
+++ binary_search_tree_test.rb
@@ -63,21 +63,21 @@ class BstTest < Minitest::Test
   end
 
   def test_iterating_over_smaller_element
-    skip
+    
     four = Bst.new 4
     four.insert 2
     assert_equal [2, 4], record_all_data(four)
   end
 
   def test_iterating_over_larger_element
-    skip
+    
     four = Bst.new 4
     four.insert 5
     assert_equal [4, 5], record_all_data(four)
   end
 
   def test_iterating_over_complex_tree
-    skip
+    
     four = Bst.new 4
     four.insert 2
     four.insert 1
@@ -89,7 +89,7 @@ class BstTest < Minitest::Test
   end
 
   def test_each_returns_enumerator_if_no_block
-    skip
+    
 
     tree = Bst.new 4
     [2, 1, 3, 6, 7, 5].each { |x| tree.insert x }
@@ -111,7 +111,7 @@ class BstTest < Minitest::Test
   # If you are curious, read more about constants on RubyDoc:
   # http://ruby-doc.org/docs/ruby-doc-bundle/UsersGuide/rg/constants.html
   def test_bookkeeping
-    skip
+    
     assert_equal 1, Bst::VERSION
   end
 end
